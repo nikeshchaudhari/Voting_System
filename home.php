75d5b0f500fcb35053d6cb88c8134c84983e558c
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-05-23 15:39:58 +0545

    voting system

---
+++ home.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <section>
+        <div class="container-fluid nav-bar">
+            <div class="row">
+                <div class="col-lg-6 col-sm-12 ">
+                    <div class="logo d-flex justify-content-lg-start justify-content-sm-center ">
+                        <a href="">
+                            <img src="https://www.news24galaxy.com/wp-content/uploads/2022/01/1200px-Election_Commission_Nepal.svg_.png"
+                                alt="" width="50px">
+                        </a>
+                    </div>
+                </div>
+                <div class=" col-lg-6 d-flex justify-content-lg-end justify-content-sm-center gap-2 ">
+                   <a class="btn login-btn mt-2" href="login.php" role="button">login</a>
+                   <a class="btn logout-btn mt-2" href="logout.php" role="button">logout</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Hero Section -->
+    <div class="container-fluid">
+        <marquee behavior="0.5" direction="left">voting system काे याे वेबसाइटमा यहाँहरूलाई हार्दिक स्वागत छ।</marquee>
+        <div class="row">
+            <div class="  col-lg-12 carasole-img mt-2">
+
+
+                <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+
+                    <div class="carousel-inner ">
+
+                        <div class="carousel-item active">
+
+                            <img src="https://ocmcm.lumbini.gov.np/media/sliders/IMG_0893_rqfCOsn.jpg.1784x600_q85_crop.jpg"
+                                class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://images.pexels.com/photos/18497559/pexels-photo-18497559/free-photo-of-road-leading-to-a-mountain-valley-hidden-by-fog.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                                class="d-block w-100 " alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://images.pexels.com/photos/15906474/pexels-photo-15906474/free-photo-of-a-man-sitting-on-a-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                                class="d-block w-100 " alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+</body>
+
+</html>
