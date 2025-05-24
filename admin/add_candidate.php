143420d949a38cfc03c4cb9caae46084a433fc77
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-05-24 14:37:54 +0545

    Voting dashboard

---
+++ admin/add_candidate.php
@@ -0,0 +1,68 @@
+<?php
+session_start();
+include("../config.php");
+
+if(!isset($_SESSION['role'])|| $_SESSION['role']!= 'admin'){
+header("Location: ../login.php");
+exit;
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Voting System || ADD_CANDIDATE</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+
+
+</head>
+
+<div class="container-fluid px-0">
+
+    <div class="row">
+        <div class="col-lg-2  col-md-2 d-flex  d-none  d-md-block">
+            <div class="side-bar ">
+                <h4><img src="https://img.icons8.com/?size=100&id=u05i13Fgasru&format=png&color=000000" alt=""
+                        width="35px">
+                    Admin Panel
+                </h4>
+                <hr>
+                <a href="dashboard.php"><i class="fa-solid fa-gauge"></i>Dashboard</a>
+                <a href="add_candidate.php"><i class="fa-solid fa-user-plus"></i>Add Candidate</a>
+                <a href="manage_candidate.php"><i class="fa-solid fa-users-gear"></i>Manage Voter</a>
+                <a href="../logout.php"><i class="fa-solid fa-right-from-bracket"></i>Logout</a>
+            </div>
+
+        </div>
+        <div class="col-lg-10  px-0 ">
+            <h4 class="d-flex justify-content-center py-3 main-candidate fw-bold ">Add Candidate</h4>
+            <div class="candidate-form mx-5">
+<form class="main-form"method="post">
+  <div class="mb-3 mx-3 ">
+    <label for="exampleInputEmail1" class="form-label">Candidate Name</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+   
+  </div>
+  <div class="mb-3 mx-3 ">
+    <label for="exampleInputPassword1" class="form-label">Party</label>
+    <input type="text" class="form-control" id="party">
+  </div>
+  
+  <button type="submit" class="btn btn-primary mx-3 d-flex justify-content-sm-center aligb-item-sm-center" >Submit</button>
+</form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
