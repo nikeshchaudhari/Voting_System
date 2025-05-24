d539aa93426ef1d7a98c265f79fe60482cc95d13
Nikesh Chaudhari <info.nikesh12@gmail.com>
2025-05-24 18:12:44 +0545

    Voting dashboard

--- index.php
+++ index.php
@@ -1,100 +1,141 @@
 <?php
 session_start();
-include "config.php";
-$error = "";
+include("config.php");
+if(!isset($_SESSION['username'])){
+    header("Location: login.php");
+    exit;
+}
 ?>
 
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
-    <title>Login - Online Voting System</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap"
+        rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
 </head>
 
-<body class="bg-light">
+<body>
+
     <section>
-        <div class="container-fluid ">
-            <div class="d-flex justify-conten-center ">
-                <div class="user">
-                    <div class="d-flex justify-conten-center ">
-                        <div class="logo-img ps-2 pt-1">
+        <div class="container-fluid nav-bar px-0">
+            <div class="row">
+                <div class="col-lg-6 col-sm-12 ">
+                    <div class="logo d-flex justify-content-lg-start justify-content-sm-center px-1 ">
+                        <a href="">
                             <img src="https://www.news24galaxy.com/wp-content/uploads/2022/01/1200px-Election_Commission_Nepal.svg_.png"
-                                alt="">
-                        </div>
+                                alt="" width="50px">
+                        </a>
                     </div>
+                </div>
+                <div class=" col-lg-6 d-flex justify-content-lg-end justify-content-sm-center gap-2 px-4">
+                    <a class="btn login-btn mt-2" href="login.php" role="button">login</a>
+                    <a class="btn logout-btn mt-2" href="signUp.php" role="button">SignUp</a>
                 </div>
             </div>
         </div>
     </section>
 
-    <section>
-        <div class="container mt-5 d-flex flex-column justify-conten-center form">
-            <h3 class="mb-4 text-center">Login Form</h3>
-            <form method="POST">
-                <div class="mb-3">
-                    <label for="name" class="form-label">username</label>
-                    <input type="text" class="form-control" id="name" placeholder="enter username" name="username">
-                </div>
+    <!-- Hero Section -->
+    <div class="container-fluid">
+        <marquee behavior="0.5" direction="left">voting system काे याे वेबसाइटमा यहाँहरूलाई हार्दिक स्वागत छ।</marquee>
+        <div class="row">
+            <div class="  col-lg-12 carasole-img mt-2">
 
-                <div class="mb-3">
-                    <label for="password" class="form-label">password</label>
-                    <input type="password" class="form-control" id="password" placeholder="password" name="password">
-                </div>
-                <input type="checkbox"> <span>Rememeber me</span><br><br>
-                <div class="text-center">
-                    <button type="submit" class="btn" name="login-btn">Login</button>
-                </div>
-                <div class="text-center pt-2">
-                    <span>Don't have any account? <span><a href="signUp.php">SignUp</a></span></span>
-                </div>
-                <div class="text-center pt-2">
-                    <span><a href="#">Forget your password?</a></span>
-                </div>
-            </form>
 
+                <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+
+                    <div class="carousel-inner ">
+
+                        <div class="carousel-item active">
+
+                            <img src="https://ocmcm.lumbini.gov.np/media/sliders/IMG_0893_rqfCOsn.jpg.1784x600_q85_crop.jpg"
+                                class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://images.pexels.com/photos/18497559/pexels-photo-18497559/free-photo-of-road-leading-to-a-mountain-valley-hidden-by-fog.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                                class="d-block w-100 " alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://images.pexels.com/photos/15906474/pexels-photo-15906474/free-photo-of-a-man-sitting-on-a-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                                class="d-block w-100 " alt="...">
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying"
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
         </div>
-    </section>
-
-<?php
-if(isset($_POST['login-btn'])){
-    $userName = $_POST['username'];
-    $password = $_POST['password']; 
-
-    $query ="SELECT * FROM users WHERE username='$userName'";
-    $data = mysqli_query($conn,$query);
-   
-    if(mysqli_num_rows($data) >0){
-        $row = mysqli_fetch_array($data);
-
-        if(password_verify($password,$row['password'])){
-            $_SESSION ['username']=$row['username'];
-            $_SESSION ['role']=$row['role'];
-
-            if($row['role']=='admin'){
-                header("Location:./admin/admin.php");
-                exit;
-            }
-            else{
-                header("Location: index.php");
-                exit;
-            }
-        }else{
-            $error ="Pass & username not match..";
-        }
-    }
-    else{
-        $error ="user not found..";
-    }
-}
-
-
-?>
-
-
+    </div>
+    <!-- view Candidate Name -->
+ <div class="container-fluid  ">
+        <div class="row"> 
+            <div class=" voter title overflow-hidden ">
+                <h3 class=" text-center mt-4 ">Choose Your Voters</h3>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid  voter-view my-4 overflow-hidden">
+        <div class="row ">
+            <div class="col-3">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        
+                    </div>
+                </div>
+            </div>
+ <div class="col-3">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                       
+                    </div>
+                </div>
+            </div>
+             <div class="col-3">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                       
+                    </div>
+                </div>
+            </div>
+             <div class="col-3">
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                       
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
 </body>
 
 </html>
